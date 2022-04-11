3c3cd3028de6cf87e0b1f5216015e417361bb1f8
mesrarM <mesrar.mohamed@gmail.com>
2022-04-11 11:45:58 +0200

    test

--- my_app_test.py
+++ my_app_test.py
@@ -6,4 +6,4 @@ def test_hello():
     response = app.test_client().get('/')
 
     assert response.status_code == 200
-    assert response.data == b'<p>Hello, World!</p>'
+    assert response.data == b'<p>Hello, Wd!</p>'
